a0fd45b8c19b092876d42a9c6e0aa8a0f156edda
pic <pic@gmail.com>
2009-11-01 21:58:42 +0000

    Functios to create and match boundary profiles. First cut.
    r338 vision/branches/book

---
+++ boundary.m
@@ -0,0 +1,18 @@
+function [ri,thi] = boundary(f, varargin)
+
+    dxy = f.edge - ones(numrows(f.edge),1)*[f.xc f.yc];
+
+    r = norm2(dxy')';
+    th = -atan2(dxy(:,2), dxy(:,1));
+    [th,k] = sort(th, 'ascend');
+    plot(th)
+    r = r(k);
+
+    if nargout == 0
+        plot(dxy(:,1), dxy(:,2), varargin{:});
+
+    else
+        thi = [0:399]'/400*pi - pi;
+        ri = interp1(th, r, thi, 'spline');
+    end
+
