cd2764399438d7f28bc2fafcfe96da29b0bd0683
pic <pic@gmail.com>
2009-11-19 23:32:09 +0000

    Fix so that arguments are width, height for non image case
    r339 vision/branches/book

--- imeshgrid.m
+++ imeshgrid.m
@@ -4,12 +4,12 @@
 function [U,V] = imeshgrid(a1, a2)
 
     if nargin == 1
-        if ndims(a1) ~= 2
+        if ndims(a1) < 2
             error('expecting an image argument');
         end
         [U,V] = meshgrid(1:numcols(a1), 1:numrows(a1));
     elseif nargin == 2
-        [U,V] = meshgrid(1:a1, 1:a2);
+        [U,V] = meshgrid(1:a2, 1:a1);
     end
         
         
