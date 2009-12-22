bc3cf900f92915c655e9524b07c7d2068ad8002c
pic <pic@gmail.com>
2009-12-22 10:40:38 +0000

    Useful graphical primitives.
    r358 vision/branches/book

--- ipaste.m
+++ ipaste.m
@@ -1,88 +1,25 @@
-%IPASTE Paste an image into an image
-%
-% OUT = IPASTE(IM, IM2, P, OPTIONS) is the image IM with the subimage IM2 
-% pasted in at the position P=[U,V].
-%
-% Options::
-% 'centre'   The pasted image is centred at P, otherwise P is the top-left 
-%            corner of the subimage in IM (default)
-% 'zero'     the coordinates of P start at zero, by default 1 is assumed
-% 'set'      IM2 overwrites the pixels in IM (default)
-% 'add'      IM2 is added to the pixels in IM
-% 'mean'     IM2 is set to the mean of pixel values in IM2 and IM
-%
-% Notes::
-% - Pixels outside the pasted in region are unaffected.
-%
-% See also ILINE.
+function c2 = ipaste(c, pattern, topleft)
 
+    % TODO: should work if c and/or pattern are color 
 
-% Copyright (C) 1993-2011, by Peter I. Corke
-%
-% This file is part of The Machine Vision Toolbox for Matlab (MVTB).
-% 
-% MVTB is free software: you can redistribute it and/or modify
-% it under the terms of the GNU Lesser General Public License as published by
-% the Free Software Foundation, either version 3 of the License, or
-% (at your option) any later version.
-% 
-% MVTB is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU Lesser General Public License for more details.
-% 
-% You should have received a copy of the GNU Leser General Public License
-% along with MVTB.  If not, see <http://www.gnu.org/licenses/>.
-
-function out = ipaste(canvas, pattern, topleft, varargin)
-
-    [h,w,nc] = size(canvas);
+    [h,w,nc] = size(c);
     [ph,pw,np] = size(pattern);
+    left = topleft(1);      % x
+    top = topleft(2);       % y
 
-    opt.centre = false;
-    opt.zero = false;
-    opt.mode = {'set', 'add', 'mean'};
-
-    opt = tb_optparse(opt, varargin);
-
-    if opt.centre
-        % specify centre of pattern not topleft
-        left = topleft(1) - floor(pw/2);
-        top = topleft(2) - floor(ph/2);
-    else
-        left = topleft(1);      % x
-        top = topleft(2);       % y
-    end
-
-    if opt.zero
-        left = left+1;
-        top = top+1;
-    end
-
-    if (top+ph-1) > h
-        error('pattern falls off bottom edge');
-    end
-    if (left+pw-1) > w
-        error('pattern falls off right edge');
+    [top left w h pw ph]
+    if (top+ph > h) || (left+pw) > w
+        error('pattern falls off edge');
     end
 
     if np > nc
         % pattern has multiple planes, replicate the canvas
-        out = repmat(canvas, [1 1 np]);
+        c2 = repmat(c, [1 1 np]);
     else
-        out = canvas;
+        c2 = c;
     end
     if np < nc
         pattern = repmat(pattern, [1 1 nc]);
     end
-    switch opt.mode
-    case 'set'
-        out(top:top+ph-1,left:left+pw-1,:) = pattern;
-    case 'add'
-        out(top:top+ph-1,left:left+pw-1,:) = out(top:top+ph-1,left:left+pw-1,:) +pattern;
-    case 'mean'
-        old = out(top:top+ph-1,left:left+pw-1,:);
-        k = ~isnan(pattern);
-        old(k) = 0.5 * (old + pattern);
-        out(top:top+ph-1,left:left+pw-1,:) = old;
-    end
+    c2(top:top+ph-1,left:left+pw-1,:) = pattern;
+        
