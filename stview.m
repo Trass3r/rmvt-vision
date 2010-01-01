714d78b58029a7764b060d271cd8b1c9b96f370f
pic <pic@gmail.com>
2010-01-01 12:41:23 +0000

    tidy up, remove debug, use idisp, add second vertical line for right image.
    r389 vision/branches/book

--- stview.m
+++ stview.m
@@ -1,16 +1,23 @@
 function stview(L, R)
 
-    image(2256*[L R]);
+    % display the images side by side
+    idisp([L R], 'nogui');
     
+    % initial cross hair location
     Y = 100;
     X = 100;
-        ud.hline = line('XData',get(gca,'XLim'),'YData',[Y Y], ...
-                     'EraseMode','xor','Tag','Horizontal Cursor');
-        ud.vline = line('XData',[X X],'YData',get(gca,'YLim'), ...
-                     'EraseMode','xor','Tag','Vertical Cursor');
+
+    % create the 3 lines segments and stash in user data on the axis
+    ud.w = size(L,2);
+    ud.hline = line('XData',get(gca,'XLim'),'YData',[Y Y], ...
+                 'EraseMode','xor','Tag','Horizontal Cursor');
+    ud.vline_l = line('XData',[X X],'YData',get(gca,'YLim'), ...
+                 'EraseMode','xor','Tag','Vertical Cursor');
+    ud.vline_r = line('XData',[X+ud.w X+ud.w],'YData',get(gca,'YLim'), ...
+                 'EraseMode','xor','Tag','Vertical Cursor');
    set(gca, 'UserData', ud);
 
-    % Set the WindowButton...Fcn of the figure
+    % Set the WindowButtonFcn of the figure
     set(gcf,'WindowButtonDownFcn', @buttonDown,...
             'WindowButtonUpFcn',@buttonUp);	
                 
@@ -19,19 +26,17 @@ end
 function moveCursor(src, event)
     ud = get(gca, 'UserData');
      cp = get(gca,'CurrentPoint');
-     cp
      set(ud.hline, 'YData', [cp(1,2) cp(2,2)]);
-     set(ud.vline, 'XData', [cp(1,1) cp(2,1)]);
+     set(ud.vline_l, 'XData', [cp(1,1) cp(2,1)]);
+     set(ud.vline_r, 'XData', ud.w+[cp(1,1) cp(2,1)]);
 end
 
 function buttonDown(src, event)
     set(gcf, 'WindowButtonMotionFcn',@moveCursor);
-disp('down');
-
+    %disp('down');
 end
 
 function buttonUp(src, event)
-disp('up');
+    %disp('up');
     set(gcf, 'WindowButtonMotionFcn','');
-
 end
