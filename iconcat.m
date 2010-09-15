b1aa6e04e8e244d7dbc5363ef3d077eba4e7a816
pic <pic@gmail.com>
2010-09-15 11:44:57 +0000

    Allow to handle color images. Optional argument is now the starting column (or row) of each concatenated image.
    r447 vision/branches/book

--- iconcat.m
+++ iconcat.m
@@ -1,4 +1,4 @@
-function out = iconcat(images, dir, bgval)
+function [out,u0] = iconcat(images, dir, bgval)
    % image is a cell array
     width = 0;
     height = 0;
@@ -10,6 +10,8 @@ function out = iconcat(images, dir, bgval)
         bgval = NaN;
     end
     
+    np_prev = NaN;
+    u0 = 1;
     for i=1:length(images)
         if dir == 'v'
             images{i} = images{i}';
@@ -17,12 +19,17 @@ function out = iconcat(images, dir, bgval)
         
         image = images{i};
 
-        [nr,nc] = size(image);
+        [nr,nc,np] = size(image);
+        if ~isnan(np_prev) && np ~= np_prev
+            error('all images must have same number of planes');
+        end
         width = width + nc;
         height = max(height, nr);
-        ud.widths(i) = width;
+        if i > 1
+            u0(i) = u0(i-1) + nc;
+        end
     end
-    composite = bgval*ones(height, width);
+    composite = bgval*ones(height, width, np);
 
     u = 1;
     for i=1:length(images)
@@ -31,7 +38,7 @@ function out = iconcat(images, dir, bgval)
     end
     
     if dir == 'v'
-        composite = composite';
+        composite = permute(composite, [2 1 3]);
     end
         
     
