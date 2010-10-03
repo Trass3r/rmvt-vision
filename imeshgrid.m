79193d7496f41e0761e75f80af7c232deec1048d
pic <pic@gmail.com>
2010-10-03 10:01:36 +0000

    Accepts a scalar or 2-vector to specify width and height.
    r478 vision/branches/book

--- imeshgrid.m
+++ imeshgrid.m
@@ -1,15 +1,23 @@
 % [U,V] = imeshgrid(image)
 % [U,V] = imeshgrid(width, height)
+% [U,V] = imeshgrid(size)
 
 function [U,V] = imeshgrid(a1, a2)
 
     if nargin == 1
-        if ndims(a1) < 2
-            error('expecting an image argument');
+        if length(a1) == 1
+            % we specified a size for a square output image
+            [U,V] = meshgrid(1:a1, 1:a1);
+        elseif length(a1) == 2
+            % we specified a size for a rectangular output image (w,h)
+            [U,V] = meshgrid(1:a1(1), 1:a1(2));
+        elseif ndims(a1) >= 2
+            [U,V] = meshgrid(1:numcols(a1), 1:numrows(a1));
+        else
+            error('incorrect argument');
         end
-        [U,V] = meshgrid(1:numcols(a1), 1:numrows(a1));
     elseif nargin == 2
-        [U,V] = meshgrid(1:a2, 1:a1);
+        [U,V] = meshgrid(1:a1, 1:a2);
     end
         
         
