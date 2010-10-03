1e0048c7c8613889e235bfa7748a26c234228587
pic <pic@gmail.com>
2010-10-03 09:49:13 +0000

    Use tb_optparse Support facepoint and centre option More elegant way that cube vertices and edges are defined.
    r472 vision/branches/book

--- mkcube.m
+++ mkcube.m
@@ -8,7 +8,7 @@
 %	[x,y,z] = MKCUBE(s)
 %	[x,y,z] = MKCUBE(s, c)
 %
-%	Return a 3-rows matrix where each row contains the (x,y,z) coordinates
+%	Return a 3-rows matrix where each column contains the (x,y,z) coordinates
 %	of a cube's vertices.  The side length S defaults to 1.  The centre
 %	C = (x,y,z) defaults to (0,0,0).
 %
@@ -19,11 +19,9 @@
 %	xyz = MKCUBE2(s)
 %	xyz = MKCUBE2(s, c)
 %
-%	Return a 6-column matrix where each row represents the edge of
-%	a cube.  Each row comprises the (x,y,z) coordinates of the start
-%	and end of each edge.
-%	The side length S defaults to 1.  The centre C = (x,y,z) defaults 
-%	to (0,0,0).
+%  [x,y,z] = mkcube(s, 'edge');
+%	Returns three matrices that can be used with mesh(x,y,z) or
+%  camera.mesh()
 %
 
 % Copyright (C) 1995-2009, by Peter I. Corke
@@ -43,32 +41,45 @@
 % You should have received a copy of the GNU Leser General Public License
 % along with MVTB.  If not, see <http://www.gnu.org/licenses/>.
 
-function [o1,o2,o3] = mkcube(s, T, opt)
-	if nargin < 1
-		s = 1;
-    end
+function [o1,o2,o3] = mkcube(s, varargin)
+    
+    opt.centre = [];
+    opt.T = [];
+    opt.edge = false;
+    opt.facepoint = false;
+
+    opt = tb_optparse(opt, varargin);
 
     % vertices of a unit cube with one corner at origin
     cube = [
-        0     0     1     1     0     0     1     1
-        0     1     0     1     0     1     0     1
-        0     0     0     0     1     1     1     1 ];
+       -1    -1     1     1    -1    -1     1     1
+       -1     1     1    -1    -1     1     1    -1
+       -1    -1    -1    -1     1     1     1     1 ];
 
+    if opt.facepoint
+        faces = [
+          1    -1     0     0     0     0
+          0     0     1    -1     0     0
+          0     0     0     0     1    -1 ];
+        cube = [cube faces];
+    end
 
-    if nargin < 3
-        % vertices of cube about the origin
-        cube = (cube - 0.5) * s;
+    % vertices of cube about the origin
+    cube = cube / 2 * s;
 
-        if nargin == 2
-            % optionally transform the vertices
-            if isempty(T)
-                T = eye(4,4);
-            elseif isvec(T)
-                T = transl(T);
-            end
-            cube = transformp(T, cube);
+    % offset it
+    if ~isempty(opt.centre)
+        cube = bsxfun(@minus, opt.centre(:));
+    end
+    % optionally transform the vertices
+    if ~isempty(opt.T)
+        if isvec(opt.T)
+            opt.T = transl(opt.T);
         end
+        cube = transformp(opt.T, cube);
+    end
 
+    if opt.edge == false
         if nargout <= 1,
             o1 = cube;
         elseif nargout == 3,
@@ -76,36 +87,9 @@ function [o1,o2,o3] = mkcube(s, T, opt)
             o2 = cube(2,:);
             o3 = cube(3,:);
         end
-    elseif nargin == 3 && strcmp(opt, 'edges')
-
-        % cube has 12 edges
-        cube = [
-            0 0 0 1 0 0
-            0 0 0 0 1 0
-            0 0 0 0 0 1
-
-            1 1 1 0 1 1
-            1 1 1 1 0 1
-            1 1 1 1 1 0
-
-            0 0 1 0 1 1
-            0 0 1 1 0 1
-
-            1 0 0 1 0 1
-            1 0 0 1 1 0
-
-            0 1 0 0 1 1
-            0 1 0 1 1 0
-        ]';
-
-        % vertices of cube about the origin
-        cube = (cube - 0.5) * s;
-
-        % optionally transform the vertices
-        if isempty(T)
-            T = eye(4,4);
-        elseif isvec(T)
-            T = transl(T);
-        end
-        o1 = [transformp(T, cube(1:3,:)); transformp(T, cube(4:6,:))];
+    else
+        cube = cube(:,[1:4 1 5:8 5]);
+        o1 = reshape(cube(1,:), 5, 2)';
+        o2 = reshape(cube(2,:), 5, 2)';
+        o3 = reshape(cube(3,:), 5, 2)';
     end
