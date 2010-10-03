20ec060672a74cb6966db0ebd772dbf8ba336f6d
pic <pic@gmail.com>
2010-10-03 09:57:41 +0000

    Create separate functions for scaling and rotating. irotate can do scaling as an option. common extrapval option
    r476 vision/branches/book

--- irotate.m
+++ irotate.m
@@ -1,41 +1,11 @@
-%IROTATE Rotate image
+%IROTATE rotate image
 %
-% OUT = IROTATE(IM, ANGLE, OPTIONS) is a version of the image IM
-% that has been rotated about its centre.
-%
-% Options::
-% 'outsize',S     set size of output image to HxW where S=[W,H]
-% 'crop'          return central part of image, same size as IM
-% 'scale',S       scale the image size by S (default 1)
-% 'extrapval',V   set background pixels to V (default 0)
-% 'smooth',S      initially smooth the image with a Gaussian of standard 
-%                 deviation S
-%
-% Notes::
-% - Rotation is defined with respect to a z-axis which is into the image.
-% - Counter-clockwise is a positive angle.
-% - The pixels in the corners of the resulting image will be undefined and
-%   set to the 'extrapval'.
-%
-% See also ISCALE.
-
-
-% Copyright (C) 1993-2011, by Peter I. Corke
-%
-% This file is part of The Machine Vision Toolbox for Matlab (MVTB).
-% 
-% MVTB is free software: you can redistribute it and/or modify
-% it under the terms of the GNU Lesser General Public License as published by
-% the Free Software Foundation, either version 3 of the License, or
-% (at your option) any later version.
-% 
-% MVTB is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU Lesser General Public License for more details.
-% 
-% You should have received a copy of the GNU Leser General Public License
-% along with MVTB.  If not, see <http://www.gnu.org/licenses/>.
+%  out = iscale(im, factor, angle)
+%       Returns an image of the same size as the input image or smaller.
+%   that has been
+%       scaled and rotated about its centre.  Rotation is defined with 
+%       respect to a z-axis into the image. Counter-clockwise is a
+%       positive angle. factor >1 is larger, <1 is smaller.
 
 function im2 = irotate(im, angle, varargin)
 
@@ -43,7 +13,6 @@ function im2 = irotate(im, angle, varargin)
     opt.crop = false;
     opt.scale = 1.0;
     opt.extrapval = 0;
-    opt.smooth = [];
     
     opt = tb_optparse(opt, varargin);
 
@@ -53,10 +22,7 @@ function im2 = irotate(im, angle, varargin)
         is_int = true;
         im = idouble(im);
     end
-
-    if ~isempty(opt.smooth)
-        im = ismooth(im, opt.smooth);
-    end
+    %im = ismooth(im, 1);
 
     [nr,nc,np] = size(im);
 
@@ -66,7 +32,7 @@ function im2 = irotate(im, angle, varargin)
 
     else
         % else from specified size
-        [Uo,Vo] = meshgrid(1:opt.outsize(1), 1:opt.outsize(2));
+        [Uo,Vo] = meshgrid(1:outsize(1), 1:outsize(2));
     end
 
 
@@ -78,8 +44,8 @@ function im2 = irotate(im, angle, varargin)
     % rotation and scale
     R = rotz(angle);
     uc = nc/2; vc = nr/2;
-    Uo2 = 1/opt.scale*(R(1,1)*(Uo-uc)+R(2,1)*(Vo-vc))+uc;
-    Vo2 = 1/opt.scale*(R(1,2)*(Uo-uc)+R(2,2)*(Vo-vc))+vc;
+    Uo2 = 1/opt.scale*(R(1,1)*(Uo-uc)+R(1,2)*(Vo-vc))+uc;
+    Vo2 = 1/opt.scale*(R(2,1)*(Uo-uc)+R(2,2)*(Vo-vc))+vc;
 
     Uo = Uo2;
     Vo = Vo2;
--- iscale.m
+++ iscale.m
@@ -1,7 +1,25 @@
-% rotation is defined with respect to a z-axis into the image.
-% CW is a positive angle
-function im2 = iscale(im, factor, angle, outsize, bgcolor)
+%ISCALE scale and rotate an image
+%
+%  out = iscale(im, factor)
+%       Returns an image that is larger or smaller than the input image
+%       according to the scale factor. >1 is larger, <1 is smaller.
+%
+%  out = iscale(im, factor, angle)
+%       Returns an image of the same size as the input image that has been
+%       scaled and rotated about its centre.  Rotation is defined with 
+%       respect to a z-axis into the image. Counter-clockwise is a
+%       positive angle.
 
+function im2 = iscale(im, factor, varargin)
+
+    outsize = [];
+    bgcolor = 0;
+    
+    opt.outsize = [];
+    opt.extrapval = 0;
+
+    opt = tb_optparse(opt, varargin);
+    
     if isfloat(im)
         is_int = false;
     else
@@ -11,42 +29,29 @@ function im2 = iscale(im, factor, angle, outsize, bgcolor)
     im = ismooth(im, 1);
 
     [nr,nc,np] = size(im);
-    if nargin < 4
-        % output image size is determined by input size and scale factor
+
+    if isempty(opt.outsize)
+        ncs = nc;
+        nrs = nr;
+    else
+        % else from specified size
+    % output image size is determined by input size and scale factor
         nrs = floor(nr*factor);
         ncs = floor(nc*factor);
-    else
-        if isempty(outsize)
-            ncs = nc;
-            nrs = nr;
-        else
-            % else from specified size
-            ncs = outsize(1);
-            nrs = outsize(2);
-        end
     end
-    if nargin < 5
-        bgcolor = 0;
-    end
+
 
     % create the coordinate matrices for warping
     [U,V] = imeshgrid(im);
     [Up,Vp] = imeshgrid(nrs, ncs);
 
-    if nargin < 3
-        % no rotation, just scale
-        Up = Up/factor;
-        Vp = Vp/factor;
-    else
-        % rotation and scale
-        R = rotz(angle);
-        uc = nc/2; vc = nr/2;
-        Vp = R(2,1)*(Up-uc)+R(2,2)*(Vp-vc)+uc;
-        Up = R(1,1)*(Up-uc)+R(1,2)*(Vp-vc)+vc;
-    end
 
+    Up = Up/factor;
+    Vp = Vp/factor;
+
+    
     for k=1:size(im,3)
-        im2(:,:,k) = interp2(U, V, im(:,:,k), Up, Vp, 'linear', bgcolor);
+        im2(:,:,k) = interp2(U, V, im(:,:,k), Up, Vp, 'linear', opt.extrapval);
     end
 
     if is_int
