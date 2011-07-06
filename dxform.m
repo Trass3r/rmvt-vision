ea4e23e842be182ed46b725b572466d97e647fac
pic <pic@gmail.com>
2011-07-06 15:16:12 +0000

    added argument parser, updated doco
    r519 vision/branches/book

--- dxform.m
+++ dxform.m
@@ -1,48 +1,60 @@
-%DISTANCEXFORM Distance transform of occupancy grid
+%DTRANSFORM Distance transform
 %
-%   dist = distancexform(world, goal)
+% DT = DTRANSFORM(IM, OPTIONS) is the distance transform of the 
+% binary image IM. The value of each output pixel is the distance (pixels)
+% to the closest set pixel.
 %
-%   Compute the distance transform for the occupancy grid, world, with
-%  respect to the specified goal point (x,y).
+% Options::
+% 'Euclidean'   use Euclidean distance (default)
+% 'cityblock'   use cityblock (Manhattan) distance
+% 'show',T      display the evolving distance transform, with a delay of T
+%               seconds between frames
 %
-%   dist = distancexform(world, goal, metric)
-%
-%  Specify the metric, either 'cityblock' or 'Euclidean'
-%
-%   dist = distancexform(world, goal, metric, show)
-%
-% Show an animation of the distance transform being formed, with a delay
-% of show seconds between frames.
+% See also IMORPH, DISTANCEXFORM, DXform.
 
-function d = distancexform(world, metric, show)
-    
-    if nargin < 3
-        show = 0;
+% Copyright (C) 1993-2011, by Peter I. Corke
+%
+% This file is part of The Robotics Toolbox for Matlab (RTB).
+% 
+% RTB is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Lesser General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% RTB is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Lesser General Public License for more details.
+% 
+% You should have received a copy of the GNU Leser General Public License
+% along with RTB.  If not, see <http://www.gnu.org/licenses/>.
+
+function d = dtransform(world, varargin)
+
+    opt.metric = {'Euclidean', 'cityblock'};
+    opt.show = [];
+
+    [opt,args] = tb_optparse(opt, varargin);
+    if length(args) > 0 && isnumeric(args{1})
+        opt.show = args{1};
     end
 
-    % set up the distance metrics
-    if nargin < 2
-        metric = 'cityblock';
-    end
-
-    if strncmpi(metric, 'cityblock', length(metric))
+    if strcmpi(opt.metric, 'cityblock')
         m = ones(3,3);
         m(2,2) = 0;
-    elseif strncmpi(metric, 'euclidean', length(metric))
+    elseif strcmpi(opt.metric, 'Euclidean')
         r2 = sqrt(2);
         m = [r2 1 r2; 1 0 1; r2 1 r2];
-    else
-        error('unknown distance metric');
     end
 
     world(world==0) = Inf;
     world(world==1) = 0;
 
     count = 0;
-    while 1,
+    while 1
         world = imorph(world, m, 'plusmin');
         count = count+1;
-        if show
+        if ~isempty(opt.show)
             cmap = gray(256);
             cmap = [1 0 0; cmap];
             colormap(cmap)
@@ -50,7 +62,7 @@ function d = distancexform(world, metric, show)
             set(gca, 'Ydir', 'normal');
             xlabel('x');
             ylabel('y');
-            pause(show);
+            pause(opt.show);
         end
 
         if length(find(world(:)==Inf)) == 0
