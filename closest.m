47c3aa7ba754f2c09e59d6bf3fbb7c176b7c2a5e
pic <pic@gmail.com>
2011-09-11 08:52:08 +0000

    add check for existence of MEX file
    r598 vision/branches/book

--- closest.m
+++ closest.m
@@ -33,3 +33,7 @@
 % You should have received a copy of the GNU Leser General Public License
 % along with MVTB.  If not, see <http://www.gnu.org/licenses/>.
 % - is a MEX file.
+
+if ~exist('closest', 3)
+    error('you need to build the MEX version of closest, see vision/mex/README');
+end
